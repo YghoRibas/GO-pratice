2ec19d33a14c8abe1ea0975b08f0fd402b21d7b4
Ribas <3rdparty_yghor.gomes@inter.co>
2024-07-01 10:39:47 -0300

    concludes say hello to multiple persons

--- first-step/greetings/greetings.go
+++ first-step/greetings/greetings.go
@@ -14,6 +14,21 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 func randomFormat() string {
 	formats := []string{
 		"Hi, %v. Welcome!",
@@ -22,6 +37,5 @@ func randomFormat() string {
 	}
 
 	index := rand.Intn(len(formats))
-	fmt.Println(index)
 	return formats[index]
 }
--- first-step/hello/hello.go
+++ first-step/hello/hello.go
@@ -11,10 +11,12 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Ribas")
+	names := []string{"Pedro", "Gabriel", "Ribas", "Lucas"}
+
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
